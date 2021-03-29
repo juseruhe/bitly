6724948c51eb635ab3dc46cfff88be8b5e8c918a
Sebastian <jsruiz7411@misena.edu.co>
2021-03-29 12:18:31 -0500

    Ya tenemos api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -5,5 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class ShortUrlService {
 
+ 
+
+  URL = ' https://api-ssl.bitly.com/v4/shorten';
+  token = '7264bbb8a5828dc30ea90dedd6af14c24998afb1'
+
   constructor() { }
 }
